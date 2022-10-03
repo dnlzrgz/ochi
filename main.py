16faf63611e48c59d1c62967909d33c3b85baf4c
daniarlert <arlertdaniel@gmail.com>
2022-10-03 19:24:37 +0200

    feat: add basic cli

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import asyncio
 import httpx
+import asyncclick as click
 from ochi import fetch_ids, fetch_stories
 from ochi.constants import BASE_URL
 
 
-async def main() -> None:
+@click.command()
+@click.option('--max', type=int, default=500, help='Max number of stories.')
+async def main(max: int) -> None:
     async with httpx.AsyncClient(base_url=BASE_URL) as c:
         ids = await fetch_ids(c)
-        stories = await fetch_stories(c, ids)
+        stories = await fetch_stories(c, ids[:max])
 
         for story in stories:
             print(story)
