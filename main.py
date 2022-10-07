36bf50a3deaa6c6310cb6138bda19d7a0abf4088
daniarlert <arlertdaniel@gmail.com>
2022-10-07 22:33:47 +0200

    feat: try-catch exceptions

--- main.py
+++ main.py
@@ -2,25 +2,34 @@ import asyncio
 
 import asyncclick as click
 import httpx
-from rich.console import Console
+from rich import print as rprint
 
-from ochi import fetch_ids, fetch_stories
-from ochi.constants import BASE_URL
+from ochi import HN_BASE_URL, fetch_ids, fetch_stories
 
 
 @click.command()
 @click.option('--max', type=int, default=500, help='Max number of stories.')
 async def main(max: int) -> None:
-    console = Console()
+    client = httpx.AsyncClient(base_url=HN_BASE_URL)
     stories = []
 
-    with console.status('[bold white]Loading stories...', spinner='point'):
-        async with httpx.AsyncClient(base_url=BASE_URL) as c:
-            ids = await fetch_ids(c)
-            stories = await fetch_stories(c, ids[:max])
+    try:
+        ids = await fetch_ids(client)
+        stories = await fetch_stories(client, ids[:max])
+    except httpx.HTTPStatusError as err:
+        rprint(
+            f"Error response {err.response.status_code} while fetching {err.request.url}"
+        )
+
+    except httpx.RequestError as err:
+        rprint(
+            f'A request error happened while fetching stories from {err.request.url}: {err}'
+        )
+    finally:
+        await client.aclose()
 
     for story in stories:
-        print(story)
+        rprint(story.pretty_str())
 
 
 if __name__ == '__main__':
